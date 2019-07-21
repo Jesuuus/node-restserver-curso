e9b8ceae38793d088ecb4a7fd999c2473ebc3e27
Jesus <jesusitovallejo@gmail.com>
2019-07-21 18:33:23 +0200

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,18 @@
 //=============
 //Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+//=============
+//Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=============
+//Base de datos
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://jesus:b8irM89osQJDvnoJ@cluster0-9bgzn.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 require('./config/config');
+//jesus b8irM89osQJDvnoJ
+//MongoDB URL
+//mongodb+srv://jesus:b8irM89osQJDvnoJ@cluster0-9bgzn.mongodb.net/cafe
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -11,36 +16,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
-})
-app.post('/usuario', function(req, res) {
+//mongoose.connect('mongodb://localhost:27017/cafe',
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
+        if (err) throw err;
 
-    let body = res.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        })
-    }
-
-})
-app.put('/usuario/:id', function(req, res) {
-    let id = res.params.id;
-    res.json({
-        id
+        console.log('Base de datos ONLINE');
     })
-})
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-})
-
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto:', 3000);
